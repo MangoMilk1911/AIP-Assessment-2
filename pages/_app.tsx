6954584b6e69ddc5b119c9a95fe60a129c24e4ad
LonJonn <leon.salsiccia@gmail.com>
2020-09-11 12:57:43 +1000

    chore: clean out template code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { AppProps } from 'next/app';
-import '../styles/globals.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
 
