83b0fd047541860a699d63183363a76c246459a5
Jyuston <justinyuen2000@hotmail.com>
2020-10-01 22:25:34 +1000

    refactor: ui for request conception

---
+++ src/components/RewardRow.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Box, Center, Heading, CloseButton, Input, IconButton, Grid } from "@chakra-ui/core";
+import { AddIcon, MinusIcon } from "@chakra-ui/icons";
+import getEmojiName from "utils/getEmojiName";
+
+interface RewardRowProps {
+  emoji: string;
+}
+const RewardRow: React.FC<RewardRowProps> = (props) => {
+  return (
+    <>
+      <Grid templateColumns="15% 40% 35% 10%" py="2rem">
+        <Heading textAlign="center">{props.emoji}</Heading>
+        <Heading textAlign="center" isTruncated>
+          {getEmojiName(props.emoji)}
+        </Heading>
+        <Grid px="1rem" templateColumns="repeat(3,1fr)">
+          <IconButton aria-label="Increment reward" icon={<MinusIcon />} />
+          <Input />
+          <IconButton aria-label="Increment reward" icon={<AddIcon />} />
+        </Grid>
+        <CloseButton />
+      </Grid>
+    </>
+  );
+};
+
+export default RewardRow;
