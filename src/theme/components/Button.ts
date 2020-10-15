c7bef87f1592e0376e8c12af5665d30446003c90
LonJonn <leon.salsiccia@gmail.com>
2020-10-15 18:24:00 +1100

    style: button styles update

--- src/theme/components/Button.ts
+++ src/theme/components/Button.ts
@@ -2,14 +2,8 @@ const baseStyle = {
   fontFamily: "button",
   fontWeight: "button",
   textTransform: "capitalize",
-  letterSpacing: 2,
-};
-
-const defaultProps = {
-  colorScheme: "primary",
 };
 
 export default {
   baseStyle,
-  defaultProps,
 };
