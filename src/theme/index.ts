b2c67e8c4af7e284d900d7ad5812575bda81dcb8
LonJonn <leon.salsiccia@gmail.com>
2020-10-15 18:23:46 +1100

    style: disable focus for non-keyboard users

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,4 +1,4 @@
-import { extendTheme } from "@chakra-ui/core";
+import { ColorModeOptions, extendTheme } from "@chakra-ui/core";
 import styles from "./styles";
 import foundation from "./foundation";
 
@@ -6,13 +6,27 @@ import Button from "./components/Button";
 import Input from "./components/Input";
 import Heading from "./components/Heading";
 
+// Hide focus box shadow when not using keyboard
+import "focus-visible/dist/focus-visible";
+
+// Color mode settings
+const config: ColorModeOptions = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
+
 // Refer to: https://github.com/chakra-ui/chakra-ui/tree/master/packages/theme/src
-export default extendTheme({
-  styles,
+const theme = extendTheme({
   ...foundation,
+  styles,
+  config,
   components: {
     Button,
     Input,
     Heading,
   },
 });
+
+export type Theme = typeof theme;
+
+export default theme;
