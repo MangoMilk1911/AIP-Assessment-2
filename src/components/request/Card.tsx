8af522758e845d511aa62d659d7f28f729c259fb
Jyuston <justinyuen2000@hotmail.com>
2020-10-16 17:22:52 +1100

    refactor: request modal

--- src/components/request/Card.tsx
+++ src/components/request/Card.tsx
@@ -1,15 +1,16 @@
-import { Box, Divider, Flex, Spacer, Text, Wrap } from "@chakra-ui/core";
-import { Contribution } from "models";
+import { Flex, Spacer, Text, useDisclosure } from "@chakra-ui/core";
 import { RequestSchema } from "models/Request";
 import React, { useMemo } from "react";
 import ReactTimeAgo from "react-time-ago";
+import ReqModal from "./ReqModal";
 
 interface RequestCardProps {
   request: RequestSchema;
 }
 
 const RequestCard: React.FC<RequestCardProps> = ({ request }) => {
-  const { noOfContributors, owner, title, createdAt } = request;
+  const { noOfContributors, owner, title, createdAt, description } = request;
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const contributors = useMemo(() => {
     const restCount = noOfContributors - 1;
@@ -20,23 +21,33 @@ const RequestCard: React.FC<RequestCardProps> = ({ request }) => {
     return contrString;
   }, [noOfContributors, owner.displayName]);
 
-  //const contributions = Contribution.findById(request._id);
   return (
-    <Flex h={48} direction="column" bg="whiteAlpha.200" borderRadius="lg" p="5" width="sm">
-      <Text fontSize="xl" fontWeight="bold" isTruncated>
-        {title}
-      </Text>
+    <>
+      <Flex
+        as="Button"
+        h={48}
+        direction="column"
+        bg="whiteAlpha.200"
+        borderRadius="lg"
+        p="5"
+        width="sm"
+        onClick={onOpen}
+      >
+        <Text fontSize="xl" fontWeight="bold" isTruncated>
+          {title}
+        </Text>
 
-      <Text>{contributors}</Text>
+        <Text>{contributors}</Text>
 
-      <Spacer />
-
-      <Flex>
-        <ReactTimeAgo date={createdAt} locale="en-US" timeStyle="round-minute" />
         <Spacer />
-        <Text>Contributors: {noOfContributors}</Text>
+
+        <Flex>
+          <ReactTimeAgo date={createdAt} locale="en-US" timeStyle="round-minute" />
+        </Flex>
       </Flex>
-    </Flex>
+
+      <ReqModal request={request} onOpen={onOpen} onClose={onClose} isOpen={isOpen}></ReqModal>
+    </>
   );
 };
 
