102facf309f78a28ef93131af4627165d9c35d71
LonJonn <leon.salsiccia@gmail.com>
2020-10-20 01:13:34 +1100

    refactor: clean up create

--- src/pages/requests/create.tsx
+++ src/pages/requests/create.tsx
@@ -1,6 +1,6 @@
-import RewardList from "@/components/reward/RewardList";
+import React, { useReducer } from "react";
+import Head from "next/head";
 import {
-  Box,
   Button,
   Container,
   FormControl,
@@ -9,13 +9,13 @@ import {
   Grid,
   Heading,
   Input,
+  Stack,
   Textarea,
 } from "@chakra-ui/core";
 import { yupResolver } from "@hookform/resolvers/yup";
+import RewardList from "components/reward/RewardList";
 import { requestValidation } from "lib/validator/schemas";
 import { Rewards } from "models/Request";
-import Head from "next/head";
-import React, { useReducer } from "react";
 import { useForm } from "react-hook-form";
 
 export type RewardsReducerState = {
@@ -65,62 +65,46 @@ const CreateRequest: React.FC = () => {
       </Head>
 
       <Container maxW="4xl">
-        <Heading as="h1" size="lg" my={5}>
+        <Heading size="lg" my={6}>
           Create Request
         </Heading>
 
-        <Box as="form" onSubmit={handleSubmit(createRequest)}>
+        <Stack as="form" onSubmit={handleSubmit(createRequest)} spacing={8} align="start">
           <FormControl isInvalid={!!formErrors.title}>
-            <FormLabel htmlFor="title" as="h2" size="md" my={5}>
-              Title
-            </FormLabel>
-            <Input type="text" name="title" id="name" placeholder="What to do" ref={register()} />
+            <FormLabel htmlFor="title">Title</FormLabel>
+            <Input name="title" id="title" placeholder="What to do" ref={register} />
             <FormErrorMessage>{formErrors.title?.message}</FormErrorMessage>
           </FormControl>
 
           <FormControl isInvalid={!!formErrors.description}>
-            <FormLabel htmlFor="description" as="h2" size="md" my={5}>
-              Description
-            </FormLabel>
+            <FormLabel htmlFor="description">Description</FormLabel>
             <Textarea
               name="description"
               id="description"
               placeholder="Add Additional Information"
-              ref={register()}
+              ref={register}
             />
             <FormErrorMessage>{formErrors.description?.message}</FormErrorMessage>
           </FormControl>
 
           <FormControl isInvalid={!!formErrors.rewards}>
-            <FormLabel htmlFor="rewards" as="h2" size="md" my={5}>
-              Rewards
-            </FormLabel>
-            <Box>
-              {Object.keys(state.rewards).map((reward) => (
-                <Box>
-                  {reward} {state.rewards[reward]}
-                </Box>
-              ))}
-            </Box>
+            <FormLabel htmlFor="rewards">Rewards</FormLabel>
             <Grid>
               <RewardList rewards={state.rewards} dispatch={dispatch} />
             </Grid>
 
             <Input
-              readOnly
-              type="text"
+              hidden
               name="rewards"
               id="rewards"
               value={JSON.stringify(state.rewards)}
-              ref={register()}
+              ref={register}
             ></Input>
             <FormErrorMessage>{formErrors.rewards?.message}</FormErrorMessage>
           </FormControl>
 
-          <Box>
-            <Button type="submit">Create</Button>
-          </Box>
-        </Box>
+          <Button type="submit">Create</Button>
+        </Stack>
       </Container>
     </>
   );
