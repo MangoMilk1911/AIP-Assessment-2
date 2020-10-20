5d2688a578e5ddb1c834d756dbb4a12628941a20
Jyuston <39510326+Jyuston@users.noreply.github.com>
2020-10-20 13:27:05 +1100

    refactor: custom reward reducer hook (#20)

---
+++ src/hooks/useRewardsReducer.ts
@@ -0,0 +1,27 @@
+import { Rewards } from "models/Favour";
+import { useReducer } from "react";
+
+type RewardsReducerAction =
+  | { type: "set"; payload: { reward: string; quantity: number } }
+  | { type: "remove"; payload: string }
+  | { type: "clear" };
+
+function rewardsReducer(state: Rewards, action: RewardsReducerAction) {
+  switch (action.type) {
+    case "set":
+      state[action.payload.reward] = action.payload.quantity;
+      return state;
+    case "remove":
+      delete state[action.payload];
+      return state;
+    case "clear":
+      return {};
+    default:
+      throw new Error("No action provided for rewards reducer.");
+  }
+}
+
+export default function useRewardList() {
+  const [rewards, dispatch] = useReducer(rewardsReducer, {});
+  return { rewards, dispatch };
+}
