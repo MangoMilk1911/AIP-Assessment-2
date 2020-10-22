e70f0818fc31175a2c256676329fd577c2ba0f29
LonJonn <leon.salsiccia@gmail.com>
2020-10-23 06:54:17 +1100

    feat: create favours from claimed request

--- src/pages/api/requests/[id]/evidence.ts
+++ src/pages/api/requests/[id]/evidence.ts
@@ -1,7 +1,7 @@
 import createHandler from "lib/routeHandler";
 import multer from "multer";
 import { promises as fs } from "fs";
-import { Request } from "models";
+import { Favour, Request, User } from "models";
 import { authMiddleware } from "lib/middleware";
 
 const handler = createHandler().use(authMiddleware);
@@ -14,11 +14,24 @@ handler.post(upload.single("evidence"), async (req, res) => {
   //setting evidence on Request
   request.evidence = await fs.readFile(req.file.path);
   request.isClaimed = true;
+  await request.save();
 
   //Clean up tmp file
   await fs.unlink(req.file.path);
 
-  await request.save();
+  const recipient = await User.findById((req as any).userId);
+
+  const { contributions } = request;
+  Array.from(contributions.keys()).forEach(async (uid) => {
+    const debtor = await User.findById(uid);
+
+    await Favour.create({
+      creator: debtor.asEmbedded(),
+      debtor: debtor.asEmbedded(),
+      recipient: recipient.asEmbedded(),
+      rewards: contributions.get(uid).rewards,
+    });
+  });
 
   res.status(204).end();
 });
