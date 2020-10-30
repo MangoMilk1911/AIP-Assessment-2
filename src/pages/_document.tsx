9784eef19ec560caff51c912d730ffbf80bfe916
LonJonn <leon.salsiccia@gmail.com>
2020-10-31 10:07:17 +1100

    style: font changes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,7 +13,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Sora:wght@300;400;500;700;800&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&display=swap"
             rel="stylesheet"
           ></link>
         </Head>
