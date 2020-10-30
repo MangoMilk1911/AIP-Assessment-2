106baaee73e41b2536acf6978373653ab307faaa
LonJonn <leon.salsiccia@gmail.com>
2020-10-31 01:24:18 +1100

    style: bg and font color changes

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -3,8 +3,8 @@ import { mode, Styles } from "@chakra-ui/theme-tools";
 const styles: Styles = {
   global: (props) => ({
     body: {
-      bg: mode("white", "gray.800")(props),
-      color: mode("primary.900", "primary.50")(props),
+      bg: mode("#fefefe", "gray.800")(props),
+      color: mode("primary.700", "primary.50")(props),
       fontFamily: "body",
       lineHeight: "base",
       transition: "0.2s ease",
