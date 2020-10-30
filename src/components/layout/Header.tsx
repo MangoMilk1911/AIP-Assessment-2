9e3ec9457372ecbecebb697dc940f2e4812fd117
LonJonn <leon.salsiccia@gmail.com>
2020-10-30 23:31:29 +1100

    refactor: clean up header

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import NextLink, { LinkProps as NextLinkProps } from "next/link";
+import {
+  Container,
+  Flex,
+  Heading,
+  LinkProps,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  useColorModeValue,
+  MenuDivider,
+  Link,
+  Stack,
+  Avatar,
+  Text,
+} from "@chakra-ui/core";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { useAuth } from "hooks/useAuth";
+
+/**
+ * Navbar Item
+ */
+type NavItemProps = NextLinkProps & LinkProps;
+
+const NavItem: React.FC<NavItemProps> = ({ children, href, ...restProps }) => (
+  <NextLink href={href} passHref>
+    <Link
+      color="inherit"
+      _hover={{
+        color: useColorModeValue("primary.500", "primary.200"),
+      }}
+      {...restProps}
+    >
+      {children}
+    </Link>
+  </NextLink>
+);
+
+/**
+ * Navbar Drop Down Menu
+ */
+interface NavDropDownProps {
+  user: firebase.User;
+  signOut: () => Promise<void>;
+}
+
+const NavDropDown: React.FC<NavDropDownProps> = ({ user, signOut }) => (
+  <Menu placement="bottom">
+    {/* Dropdown Button */}
+    <MenuButton>
+      <Stack direction="row" align="center">
+        <Avatar boxSize={10} name={user.displayName} src={user.photoURL} />
+        <ChevronDownIcon />
+      </Stack>
+    </MenuButton>
+
+    {/* Dropdown Content */}
+    <MenuList>
+      <MenuItem _hover={{ bg: "inherit", cursor: "default" }} _focus={{ bg: "inherit" }}>
+        Welcome,{" "}
+        <Text fontWeight="semibold" ml={1}>
+          {user.displayName}
+        </Text>
+      </MenuItem>
+      <MenuDivider />
+      <MenuItem onClick={signOut}>Log Out</MenuItem>
+    </MenuList>
+  </Menu>
+);
+/**
+ * Header
+ */
+const Header: React.FC = () => {
+  const { user, signOut } = useAuth();
+
+  return (
+    <Container as="header" maxW="lg" mb={4}>
+      <Flex as="nav" justify="space-between" align="center" h={20}>
+        {/* Navbar Left */}
+        <Stack direction="row" spacing={8}>
+          <NavItem href="/">
+            <Heading size="md">Pinki</Heading>
+          </NavItem>
+          <NavItem href="/requests">Requests</NavItem>
+          <NavItem href="/favours">Favours</NavItem>
+          <NavItem href="/leaderboard">Leaderboard</NavItem>
+        </Stack>
+
+        {/* Navbar Right */}
+        {user ? (
+          <NavDropDown user={user} signOut={signOut} />
+        ) : (
+          <NavItem href="/login">Log In</NavItem>
+        )}
+      </Flex>
+    </Container>
+  );
+};
+
+export default Header;
