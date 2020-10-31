f92d193049df2839e714f89f5582e941263603e3
LonJonn <leon.salsiccia@gmail.com>
2020-10-31 18:58:26 +1100

    refactor: finish SelectUser component

--- src/components/favour/SelectUser.tsx
+++ src/components/favour/SelectUser.tsx
@@ -10,6 +10,7 @@ import {
   Spinner,
   Stack,
   Text,
+  useColorModeValue,
 } from "@chakra-ui/core";
 import { useAuth } from "hooks/useAuth";
 import useDebounce from "hooks/useDebounce";
@@ -18,27 +19,14 @@ import { useMemo, useState } from "react";
 import useSWR from "swr";
 import { motion } from "framer-motion";
 
-const listAnimation = {
-  hidden: { opacity: 0 },
-  show: {
-    opacity: 1,
-    transition: {
-      // delay: 0.2,
-      // duration: 0.2,
-      delayChildren: 0.3,
-      staggerChildren: 0.3,
-    },
-  },
-};
+/**
+ * User Preview Card
+ */
 
 const cardAnimation = {
   hidden: { opacity: 0 },
   show: { opacity: 1 },
 };
-
-/**
- * User Preview Card
- */
 
 interface UserPreviewProps {
   user: UserSchema;
@@ -52,17 +40,17 @@ const UserPreview: React.FC<UserPreviewProps> = ({ user, onClick }) => (
     direction="row"
     spacing={4}
     p={5}
-    bg="whiteAlpha.100"
+    bg={useColorModeValue("gray.200", "whiteAlpha.200")}
     align="center"
     borderRadius="lg"
     style={{ transition: "background 0.2s ease" }}
-    _hover={{ cursor: "pointer", bg: "whiteAlpha.200" }}
+    _hover={{ cursor: "pointer", bg: useColorModeValue("gray.300", "whiteAlpha.300") }}
     variants={cardAnimation}
   >
     <Avatar name={user.displayName} />
     <Box>
       <Text fontSize="xl">{user.displayName}</Text>
-      <Text color="gray.400">{user.email}</Text>
+      <Text color={useColorModeValue("gray.700", "gray.400")}>{user.email}</Text>
     </Box>
   </Stack>
 );
@@ -70,6 +58,16 @@ const UserPreview: React.FC<UserPreviewProps> = ({ user, onClick }) => (
 /**
  * Select User
  */
+
+const listAnimation = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.3,
+    },
+  },
+};
 
 interface SelectUserProps {
   onSelectUser: (user: UserSchema) => void;
@@ -91,7 +89,7 @@ const SelectUser: React.FC<SelectUserProps> = ({ onSelectUser }) => {
   }, [data]);
 
   return (
-    <Stack pos="relative">
+    <Stack>
       {/* Search Bar */}
       <Input
         value={userQuery}
@@ -99,53 +97,48 @@ const SelectUser: React.FC<SelectUserProps> = ({ onSelectUser }) => {
           setUserQuery(e.target.value);
         }}
         onFocus={() => setShow(true)}
+        onBlur={() => {
+          // Delay hide so Card onClick can register (kinda gross but...)
+          setTimeout(() => setShow(false), 200);
+        }}
         placeholder="Search for user by name"
       />
       <FormHelperText fontSize="sm">Search must be more than 2 characters.</FormHelperText>
 
       {/* Popup */}
-      <Box
-        pos="absolute"
-        top="100%"
-        zIndex="9999"
-        w="full"
-        bgColor="gray.700"
-        borderRadius="lg"
-        shadow="lg"
-      >
-        <Collapse in={show}>
-          {isValidating ? (
-            <Center py={6}>
-              <Spinner size="lg" />
-            </Center>
-          ) : !users || users?.length === 0 ? (
-            <Text fontSize="xl" textAlign="center" py={6}>
-              No users found 😢
-            </Text>
-          ) : (
-            // Cards
-            <Stack
-              as={motion.div}
-              spacing={4}
-              p={3}
-              variants={listAnimation}
-              animate={show ? "show" : "hidden"}
-            >
-              {users.map((user) => (
-                <UserPreview
-                  user={user}
-                  onClick={() => {
-                    onSelectUser(user);
-                    setUserQuery(user.displayName);
-                    setShow(false);
-                  }}
-                  key={user._id}
-                />
-              ))}
-            </Stack>
-          )}
-        </Collapse>
-      </Box>
+
+      <Collapse in={show}>
+        {isValidating ? (
+          <Center py={6}>
+            <Spinner size="lg" />
+          </Center>
+        ) : !users || users?.length === 0 ? (
+          <Text fontSize="xl" textAlign="center" py={6}>
+            No users found 😢
+          </Text>
+        ) : (
+          // Card List
+          <Stack
+            as={motion.div}
+            spacing={4}
+            p={4}
+            variants={listAnimation}
+            initial="hidden"
+            animate={show ? "show" : "hidden"}
+          >
+            {users.map((user) => (
+              <UserPreview
+                user={user}
+                onClick={() => {
+                  onSelectUser(user);
+                  setUserQuery(user.displayName);
+                }}
+                key={user._id}
+              />
+            ))}
+          </Stack>
+        )}
+      </Collapse>
     </Stack>
   );
 };
