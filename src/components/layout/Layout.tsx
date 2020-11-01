7789bd5d5d98d11fdbb5bed8e57a734571eeae4f
LonJonn <leon.salsiccia@gmail.com>
2020-11-02 10:07:21 +1100

    fix: correct types for layout props

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,11 @@
-import { Container, ContainerProps } from "@chakra-ui/core";
+import { ComponentWithAs, Container, ContainerProps } from "@chakra-ui/core";
 import Head from "next/head";
 
-interface LayoutProps {
+interface LayoutProps extends ContainerProps {
   title?: string;
 }
 
-const Layout: React.FC<LayoutProps & ContainerProps> = ({
+const Layout: ComponentWithAs<"div", LayoutProps> = ({
   title,
   maxW = "lg",
   children,
@@ -18,6 +18,7 @@ const Layout: React.FC<LayoutProps & ContainerProps> = ({
       </Head>
     )}
 
+    {/* @ts-ignore issue with chakra types */}
     <Container maxW={maxW} flexGrow={1} {...restProps}>
       {children}
     </Container>
