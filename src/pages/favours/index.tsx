682482ff75c589eac3027c96c7d4174e216d2949
LonJonn <leon.salsiccia@gmail.com>
2020-11-02 12:43:17 +1100

    refactor: extract favour card

--- src/pages/favours/index.tsx
+++ src/pages/favours/index.tsx
@@ -74,6 +74,40 @@ const FavourCardTitle: React.FC<FavourCardTitleProps> = ({ debtor, recipient }) 
 };
 
 /**
+ * Favour Card
+ */
+
+interface FavourCardprops {
+  favour: FavourSchema;
+}
+
+const FavourCard: React.FC<FavourCardprops> = ({ favour }) => (
+  <Card href={`/favours/${favour._id}`} h={44}>
+    {/* Title */}
+    <FavourCardTitle debtor={favour.debtor} recipient={favour.recipient} />
+
+    {/* Rewards */}
+    <Text mt={1} fontSize="3xl">
+      {Object.keys(favour.rewards).map((reward) => (
+        <span key={reward}>{reward}</span>
+      ))}
+    </Text>
+
+    <Spacer />
+
+    {/* Bottom */}
+    <Flex align="center" justify="space-between">
+      <Text>{dayjs(favour.createdAt).from(new Date())}</Text>
+      {favour.evidence && (
+        <Badge colorScheme="green" border="1px solid">
+          Claimed
+        </Badge>
+      )}
+    </Flex>
+  </Card>
+);
+
+/**
  * Favour List
  */
 
@@ -120,29 +154,7 @@ const ListContent: React.FC<ListContentProps> = ({
           key={pageIndex} // So the animation replays whenever the page changes
         >
           {favours.map((favour) => (
-            <Card href={`/favours/${favour._id}`} h={44} key={favour._id.toString()}>
-              {/* Title */}
-              <FavourCardTitle debtor={favour.debtor} recipient={favour.recipient} />
-
-              {/* Rewards */}
-              <Text mt={1} fontSize="3xl">
-                {Object.keys(favour.rewards).map((reward) => (
-                  <span key={reward}>{reward}</span>
-                ))}
-              </Text>
-
-              <Spacer />
-
-              {/* Bottom */}
-              <Flex align="center" justify="space-between">
-                <Text>{dayjs(favour.createdAt).from(new Date())}</Text>
-                {favour.evidence && (
-                  <Badge colorScheme="green" border="1px solid">
-                    Claimed
-                  </Badge>
-                )}
-              </Flex>
-            </Card>
+            <FavourCard favour={favour} key={favour._id.toString()} />
           ))}
         </SimpleGrid>
       </AnimatePresence>
